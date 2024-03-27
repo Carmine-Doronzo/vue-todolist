8e1367ca6044470c412b020d2e33d94c863a9ee2
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-27 15:14:26 +0100

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+console.log('hello')
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        todos :
+        [
+            {
+                text:'fai la spesa',
+                done: true
+            },
+            {
+                text:'mangia',
+                done: false
+            },
+            {
+                text:'studia',
+                done: true
+            },
+            {
+                text:'leggi la documentazione',
+                done: false
+            },
+            {
+                text:'allenati',
+                done: true
+            },
+            {
+                text:'non arrabbiarti',
+                done: false
+            },
+            {
+                text:'riposati',
+                done: true
+            },
+        ]
+      }
+    }
+  }).mount('#app')
