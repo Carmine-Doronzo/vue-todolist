1019492a2cec7ddec0d933b581467e0d1024125e
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-27 17:45:31 +0100

    ADD control for empty string

--- js/script.js
+++ js/script.js
@@ -7,48 +7,59 @@ const { createApp } = Vue
         inputDo:'',
         todos :
         [
-            {
-                text:'fai la spesa',
-                done: true
-            },
-            {
-                text:'mangia',
-                done: false
-            },
-            {
-                text:'studia',
-                done: true
-            },
-            {
-                text:'leggi la documentazione',
-                done: false
-            },
-            {
-                text:'allenati',
-                done: true
-            },
-            {
-                text:'non arrabbiarti',
-                done: false
-            },
-            {
-                text:'riposati',
-                done: true
-            },
+            // {
+            //     text:'fai la spesa',
+            //     done: true
+            // },
+            // {
+            //     text:'mangia',
+            //     done: false
+            // },
+            // {
+            //     text:'studia',
+            //     done: true
+            // },
+            // {
+            //     text:'leggi la documentazione',
+            //     done: false
+            // },
+            // {
+            //     text:'allenati',
+            //     done: true
+            // },
+            // {
+            //     text:'non arrabbiarti',
+            //     done: false
+            // },
+            // {
+            //     text:'riposati',
+            //     done: true
+            // },
         ]
+        
       }
+      
     },
 
     methods:{
         remove(item,i){
             item = this.todos
             item.splice(i,1)
+            console.log(this.todos)
         },
 
         addDo(){
+            if(this.inputDo !== ''){
             this.todos.push({text:this.inputDo,done:false})
+            let alertEl = document.querySelector('.alert')
+            alertEl.innerHTML=''
+            }else{
+                let alertEl = document.querySelector('.alert')
+                alertEl.innerHTML=`Inserisci del testo corretto`
+            }
             
             this.inputDo = ''
+            
         },
         
         iDo(item){
@@ -58,8 +69,14 @@ const { createApp } = Vue
                 item.done = false
             }
             
-        }
+        },
+        
 
 
+
+
+    },
+    mounted(){
+        console.log(this.todos.length)
     }
   }).mount('#app')
